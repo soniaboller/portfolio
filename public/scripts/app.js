b6f8f9ae7701b0b453938189d88867a6e7d82d05
soniaboller <soniaboller@gmail.com>
2018-11-29 12:41:50 -0500

    updated with new work

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -70,6 +70,21 @@ app.projects = {
         link: '',
         information: 'Design collaboration with <a href="https://www.linkedin.com/in/ian-fried-92a9265a">Ian Fried </a> to create stickers promoting bicycling and biker awareness',
         technology: 'Illustrator, Photoshop'
+    },
+    oceanOfTrash: {
+        link: '',
+        information: 'As of 2015 there were 5.25 trillion pieces of plastic debris in the ocean. Ocean of Trash serves as an exploration of the impact trash has had on the ocean by recreating an abstract rendition of the ocean made entirely out of garbage or recycled items.',
+        technology: 'openFrameworks, geared DC motors, recycled wood, recycled bike inner tubes, recycled computer monitors'
+    },
+    seasonCollages: {
+        link: 'https://www.instagram.com/p/BpR-LKolQjP/',
+        information: 'Images scraped from shutter with the search terms spring, summer, autumn, winter, and run  through t-SNE in openFrameworks.',
+        technology: 'openFrameworks, Python'
+    },
+    vaginaVortex: {
+        link: 'https://www.instagram.com/p/Bo9ZeCFldlS/',
+        information: 'vagina vortex, according to urban dictionary — the force that pulls a friend away at the beginning of his new relationship, rarely losing its grip. <br/> vagina vortex, translated — the overwhelming fear men feel in regards to women sucking away all their power.',
+        technology: 'openFrameworks, Photography'
     }
 };
 // app.icons = {
@@ -139,7 +154,7 @@ $(document).ready(function() {
             }
             $('#skills').velocity('fadeIn', { delay: 750, duration: 750});
             $('#skills-list').typed({
-                strings: ['INTERACTIVE MEDIA','COMPUTER GRAPHICS ART', 'CREATIVE CODING', 'ROCK CLIMBING',' PHOTOGRAPHY', 'DOGS'],
+                strings: ['INTERACTIVE MEDIA','GENERATIVE ART', 'AUDIO VISUALIZATION', 'CREATIVE TECHNOLOGY', ' PHOTOGRAPHY', 'ROCK CLIMBING'],
                 typeSpeed: -500,
                 startDelay: 1500,
                 backDelay: 500,
@@ -238,92 +253,208 @@ $(document).ready(function() {
             var backgroundColor = $('body')[0].style.backgroundColor;
             $('#modal').css('backgroundColor', backgroundColor);
             $('#page-wrapper').addClass('modalOpen');
-            let bike = false;
+            let ocean = false;
+            let seasons = false;
             var self = this;
             var projectLink, projectInformation, projectTechnology;
             var imageSrc = $(self)[0].src;
             var classList =$(self)[0].classList;
             console.log(classList);
-            if($(self).hasClass('bike')){
-                bike = true;
+            if($(self).hasClass('ocean')){
+                ocean = true;
+            }
+            if($(self).hasClass('seasons')){
+                seasons = true;
             }
             var imageName = $(self)[0].alt;
-            if (imageName == 'COLOR THEORY'){
-                projectLink = app.projects.colorTheory.link;
-                projectInformation = app.projects.colorTheory.information;
-                projectTechnology = app.projects.colorTheory.technology;
+            switch (imageName){
+                case 'COLOR THEORY':
+                    projectLink = app.projects.colorTheory.link;
+                    projectInformation = app.projects.colorTheory.information;
+                    projectTechnology = app.projects.colorTheory.technology;
+                    break;
+                case 'CAMERALESS CONCERTS':
+                    projectLink = app.projects.cameralessConcerts.link;
+                    projectInformation = app.projects.cameralessConcerts.information;
+                    projectTechnology = app.projects.cameralessConcerts.technology;
+                    break;
+                case 'AUDIBLE VISUALS':
+                    projectLink = app.projects.audibleVisuals.link;
+                    projectInformation = app.projects.audibleVisuals.information;
+                    projectTechnology = app.projects.audibleVisuals.technology;
+                    break;
+                case 'TIC TAC NOPE':
+                    projectLink = app.projects.ticTacToe.link;
+                    projectInformation = app.projects.ticTacToe.information;
+                    projectTechnology = app.projects.ticTacToe.technology;
+                    break;
+                case 'VISUALIZER EXPERIMENTS':
+                    projectLink = app.projects.visualizerExperiments.link;
+                    projectInformation = app.projects.visualizerExperiments.information;
+                    projectTechnology = app.projects.visualizerExperiments.technology;
+                    break;
+                // case 'BEER CRAWL':
+                //     projectLink = app.projects.beerCrawl.link;
+                //     projectInformation = app.projects.beerCrawl.information;
+                //     projectTechnology = app.projects.beerCrawl.technology;
+                //     break;
+                case 'WAVE PAINTING':
+                    projectLink = app.projects.wavePainting.link;
+                    projectInformation = app.projects.wavePainting.information;
+                    projectTechnology = app.projects.wavePainting.technology;
+                    break;
+                case 'GEOMETRIC TUNEAGE':
+                    projectLink = app.projects.geometricTunage.link;
+                    projectInformation = app.projects.geometricTunage.information;
+                    projectTechnology = app.projects.geometricTunage.technology;
+                    break;
+                case 'METEORITE LANDINGS':
+                    projectLink = app.projects.meteoriteLandings.link;
+                    projectInformation = app.projects.meteoriteLandings.information;
+                    projectTechnology = app.projects.meteoriteLandings.technology;
+                    break;
+                case 'SQUARE WAVE':
+                    projectLink = app.projects.squarewave.link;
+                    projectInformation = app.projects.squarewave.information;
+                    projectTechnology = app.projects.squarewave.technology;
+                    break;
+                case 'SHADERS':
+                    projectLink = app.projects.shaders.link;
+                    projectInformation = app.projects.shaders.information;
+                    projectTechnology = app.projects.shaders.technology;
+                    break;
+                case 'BIRTHDAY':
+                    projectLink = app.projects.birthday.link;
+                    projectInformation = app.projects.birthday.information;
+                    projectTechnology = app.projects.birthday.technology;
+                    break;
+                case 'BIKE STICKERS':
+                    projectLink = '';
+                    projectInformation = app.projects.bikeStickers.information;
+                    projectTechnology = app.projects.bikeStickers.technology;
+                    break;
+                case 'OCEAN OF TRASH':
+                    projectLink = app.projects.oceanOfTrash.link;
+                    projectInformation = app.projects.oceanOfTrash.information;
+                    projectTechnology = app.projects.oceanOfTrash.technology;
+                    break;
+                case 'SEASON COLLAGES':
+                    projectLink = app.projects.seasonCollages.link;
+                    projectInformation = app.projects.seasonCollages.information;
+                    projectTechnology = app.projects.seasonCollages.technology;
+                    break;
+                case 'VAGINA VORTEX':
+                    projectLink = app.projects.vaginaVortex.link;
+                    projectInformation = app.projects.vaginaVortex.information;
+                    projectTechnology = app.projects.vaginaVortex.technology;
+                    break;
             }
-            else if (imageName == 'CAMERALESS CONCERTS'){
-                projectLink = app.projects.cameralessConcerts.link;
-                projectInformation = app.projects.cameralessConcerts.information;
-                projectTechnology = app.projects.cameralessConcerts.technology;
-            }
-            else if (imageName == 'AUDIBLE VISUALS'){
-                projectLink = app.projects.audibleVisuals.link;
-                projectInformation = app.projects.audibleVisuals.information;
-                projectTechnology = app.projects.audibleVisuals.technology;
-            }
-            else if (imageName == 'TIC TAC NOPE'){
-                projectLink = app.projects.ticTacToe.link;
-                projectInformation = app.projects.ticTacToe.information;
-                projectTechnology = app.projects.ticTacToe.technology;
-            }
-            else if (imageName == 'VISUALIZER EXPERIMENTS'){
-                projectLink = app.projects.visualizerExperiments.link;
-                projectInformation = app.projects.visualizerExperiments.information;
-                projectTechnology = app.projects.visualizerExperiments.technology;
-            }
-            else if (imageName == 'BEER CRAWL'){
-                projectLink = app.projects.beerCrawl.link;
-                projectInformation = app.projects.beerCrawl.information;
-                projectTechnology = app.projects.beerCrawl.technology;
-            }
-            else if (imageName == 'WAVE PAINTING'){
-                projectLink = app.projects.wavePainting.link;
-                projectInformation = app.projects.wavePainting.information;
-                projectTechnology = app.projects.wavePainting.technology;
-            }
-            else if (imageName == 'GEOMETRIC TUNEAGE'){
-                projectLink = app.projects.geometricTunage.link;
-                projectInformation = app.projects.geometricTunage.information;
-                projectTechnology = app.projects.geometricTunage.technology;
-            }
-            else if (imageName == 'METEORITE LANDINGS'){
-                projectLink = app.projects.meteoriteLandings.link;
-                projectInformation = app.projects.meteoriteLandings.information;
-                projectTechnology = app.projects.meteoriteLandings.technology;
-            }
-            else if (imageName == 'SQUARE WAVE'){
-                projectLink = app.projects.squarewave.link;
-                projectInformation = app.projects.squarewave.information;
-                projectTechnology = app.projects.squarewave.technology;
-            }
-            else if (imageName == 'SHADERS'){
-                projectLink = app.projects.shaders.link;
-                projectInformation = app.projects.shaders.information;
-                projectTechnology = app.projects.shaders.technology;
-            }
-            else if (imageName == 'BIRTHDAY'){
-                projectLink = app.projects.birthday.link;
-                projectInformation = app.projects.birthday.information;
-                projectTechnology = app.projects.birthday.technology;
-            }
-            else if (imageName == 'BIKE STICKERS'){
-                projectLink = '';
-                projectInformation = app.projects.bikeStickers.information;
-                projectTechnology = app.projects.bikeStickers.technology;
-            }
-            if(bike){
+            // if (imageName == 'COLOR THEORY'){
+            //     projectLink = app.projects.colorTheory.link;
+            //     projectInformation = app.projects.colorTheory.information;
+            //     projectTechnology = app.projects.colorTheory.technology;
+            // }
+            // else if (imageName == 'CAMERALESS CONCERTS'){
+            //     projectLink = app.projects.cameralessConcerts.link;
+            //     projectInformation = app.projects.cameralessConcerts.information;
+            //     projectTechnology = app.projects.cameralessConcerts.technology;
+            // }
+            // else if (imageName == 'AUDIBLE VISUALS'){
+            //     projectLink = app.projects.audibleVisuals.link;
+            //     projectInformation = app.projects.audibleVisuals.information;
+            //     projectTechnology = app.projects.audibleVisuals.technology;
+            // }
+            // else if (imageName == 'TIC TAC NOPE'){
+            //     projectLink = app.projects.ticTacToe.link;
+            //     projectInformation = app.projects.ticTacToe.information;
+            //     projectTechnology = app.projects.ticTacToe.technology;
+            // }
+            // else if (imageName == 'VISUALIZER EXPERIMENTS'){
+            //     projectLink = app.projects.visualizerExperiments.link;
+            //     projectInformation = app.projects.visualizerExperiments.information;
+            //     projectTechnology = app.projects.visualizerExperiments.technology;
+            // }
+            // // else if (imageName == 'BEER CRAWL'){
+            // //     projectLink = app.projects.beerCrawl.link;
+            // //     projectInformation = app.projects.beerCrawl.information;
+            // //     projectTechnology = app.projects.beerCrawl.technology;
+            // // }
+            // else if (imageName == 'WAVE PAINTING'){
+            //     projectLink = app.projects.wavePainting.link;
+            //     projectInformation = app.projects.wavePainting.information;
+            //     projectTechnology = app.projects.wavePainting.technology;
+            // }
+            // else if (imageName == 'GEOMETRIC TUNEAGE'){
+            //     projectLink = app.projects.geometricTunage.link;
+            //     projectInformation = app.projects.geometricTunage.information;
+            //     projectTechnology = app.projects.geometricTunage.technology;
+            // }
+            // else if (imageName == 'METEORITE LANDINGS'){
+            //     projectLink = app.projects.meteoriteLandings.link;
+            //     projectInformation = app.projects.meteoriteLandings.information;
+            //     projectTechnology = app.projects.meteoriteLandings.technology;
+            // }
+            // else if (imageName == 'SQUARE WAVE'){
+            //     projectLink = app.projects.squarewave.link;
+            //     projectInformation = app.projects.squarewave.information;
+            //     projectTechnology = app.projects.squarewave.technology;
+            // }
+            // else if (imageName == 'SHADERS'){
+            //     projectLink = app.projects.shaders.link;
+            //     projectInformation = app.projects.shaders.information;
+            //     projectTechnology = app.projects.shaders.technology;
+            // }
+            // else if (imageName == 'BIRTHDAY'){
+            //     projectLink = app.projects.birthday.link;
+            //     projectInformation = app.projects.birthday.information;
+            //     projectTechnology = app.projects.birthday.technology;
+            // }
+            // else if (imageName == 'BIKE STICKERS'){
+            //     projectLink = '';
+            //     projectInformation = app.projects.bikeStickers.information;
+            //     projectTechnology = app.projects.bikeStickers.technology;
+            // }
+            // else if (imageName == 'OCEAN OF TRASH'){
+            //     projectLink = app.projects.oceanOfTrash.link;
+            //     projectInformation = app.projects.bikeStickers.information;
+            //     projectTechnology = app.projects.bikeStickers.technology;
+            // }
+            if(seasons){
                 $('#modal').append('<span id="close-button">x</span>')
                     .append('<h4>' + imageName + '</h4>')
-                    .append('<div>'+ '<img src="' + imageSrc + '">' + '</div>')
+                    .append(
+                        '<a style="cursor: pointer;" class="big-image" href="'+ projectLink +'" target="_blank">' +
+                        '<div>'+ '<img class="collage link-image" src="/images/projects/season-collages/spring-small.png">' + '</div>' +
+                        '<div>'+ '<img class="collage link-image" src="/images/projects/season-collages/summer-small.png">' + '</div>' +
+                        '<div>'+ '<img class="collage link-image" src="/images/projects/season-collages/autumn-small.png">' + '</div>' +
+                        '<div>'+ '<img class="collage link-image" src="/images/projects/season-collages/winter-small.png">' + '</div>' +
+                        '</a>'
+                    )
                     .append('<p>' + projectInformation + '</p>')
-                    .append('<p> Created using: ' + projectTechnology + '</p>');
+                    .append('<p> Built using: ' + projectTechnology + '</p>');
+            }
+            else if(ocean){
+                $('#modal').append('<span id="close-button">x</span>')
+                    .append('<h4>' + imageName + '</h4>')
+                    .append(
+                        '<div>'+ '<img src="' + imageSrc + '">' +
+                        '<div>'+ '<video muted controls src="/images/projects/ocean-of-trash.mov">' + '</div>' +
+                        '<div>'+ '<video muted controls src="/images/projects/ocean-of-trash2.mov">' + '</div>'
+                    )
+                    .append('<p>' + projectInformation + '</p>')
+                    .append('<p> Built using: ' + projectTechnology + '</p>');
+            }
+            else if (projectLink == ''){
+                $('#modal').append('<span id="close-button">x</span>')
+                    .append('<h4>' + imageName + '</h4>')
+                    .append('<img src="' + imageSrc + '">')
+                    .append('<p>' + projectInformation + '</p>')
+                    .append('<p> Built using: ' + projectTechnology + '</p>');
             }
             else{
                 $('#modal').append('<span id="close-button">x</span>')
                     .append('<h4>' + imageName + '</h4>')
-                    .append('<a style="cursor: pointer;" href="'+ projectLink +'" target="_blank">'+ '<img src="' + imageSrc + '">' + '</a>')
+                    .append('<a style="cursor: pointer;" class="link-image" href="'+ projectLink +'" target="_blank">'+ '<img src="' + imageSrc + '">' + '</a>')
                     .append('<p>' + projectInformation + '</p>')
                     .append('<p> Built using: ' + projectTechnology + '</p>');
             }
